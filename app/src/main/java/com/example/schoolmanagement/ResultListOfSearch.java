6e995e9139814c4702b72b394a54e7063fe53b2f
Audrey <viriotaudrey@gmail.com>
2019-03-01 16:30:01 +0100

    01.03.2019 -> Navigation ok

--- app/src/main/java/com/example/schoolmanagement/ResultListOfSearch.java
+++ app/src/main/java/com/example/schoolmanagement/ResultListOfSearch.java
@@ -1,7 +1,10 @@
 package com.example.schoolmanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,10 +18,26 @@ public class ResultListOfSearch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_list_of_search);
-        classArray = new String[]{"XML","SQL","Entreprise","Anglais","Communication d'entreprise","JAVA","SAP","Marketing","Français","Math","Software development mobile de la mort qui tue de sa chienne de pute","Algo","TB","HES patron"};
+        classArray = new String[]{"XML","SQL","Entreprise","Anglais","Communication d'entreprise","JAVA","SAP","Marketing","Français"};
         listView = (ListView) findViewById(R.id.main_listview);
 
         classAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, classArray);
         listView.setAdapter(classAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.admin_settings_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void deleteButton(View view){
+        Intent intent = new Intent(this, DeleteClass.class);
+        startActivity(intent);
+    }
+
+    public void modifyButton(View view){
+        Intent intent = new Intent(this, CreateClass.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/schoolmanagement/studentsSettings.java
+++ app/src/main/java/com/example/schoolmanagement/studentsSettings.java
@@ -1,8 +1,10 @@
 package com.example.schoolmanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class studentsSettings extends AppCompatActivity {
 
@@ -16,5 +18,15 @@ public class studentsSettings extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.admin_settings_actions, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void searchStudentButton(View view){
+        Intent intent = new Intent(this, ResultListOfSearch.class);
+        startActivity(intent);
+    }
+
+    public void addStudentButton(View view){
+        Intent intent = new Intent(this, CreateClass.class);
+        startActivity(intent);
     }
 }
