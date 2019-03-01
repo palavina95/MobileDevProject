15bed9aa69e78d2543122bf8a094329152af2cd3
palavina95 <vivian.bridy@students.hevs.ch>
2019-03-01 13:55:54 +0100

    01.03.2019 -> Admin Settings Creation

--- app/src/main/java/com/example/schoolmanagement/AdminSettings.java
+++ app/src/main/java/com/example/schoolmanagement/AdminSettings.java
@@ -2,6 +2,7 @@ package com.example.schoolmanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class AdminSettings extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class AdminSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_settings);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.admin_settings_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/example/schoolmanagement/MainActivity.java
+++ app/src/main/java/com/example/schoolmanagement/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.schoolmanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logUser(View view){
+        Intent intent = new Intent(this, AdminSettings.class);
+        /*EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);*/
+        startActivity(intent);
     }
 }
