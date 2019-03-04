6b8971689166ffadbb8d395ee3e0f7b01483e9e1
palavina95 <vivian.bridy@students.hevs.ch>
2019-03-04 09:41:22 +0100

    04.03.2019 -> creation infosApp et settings icon en blanc (attention name toujours black)

--- app/src/main/java/com/example/schoolmanagement/Settings.java
+++ app/src/main/java/com/example/schoolmanagement/Settings.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -16,6 +17,7 @@ import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -52,7 +54,56 @@ public class Settings extends AppCompatActivity /*implements OnMapReadyCallback*
         setContentView(R.layout.activity_settings);
 
         getLocationPermission();
+
+
+        //Code pour cliquer sur le numéro de téléphone et le composer directement
+        final TextView phoneNumber = (TextView) findViewById(R.id.phoneNumber);
+        phoneNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                String temp = "tel:"+phoneNumber.getText();
+                intent.setData(Uri.parse(temp));
+
+                startActivity(intent);
+            }
+        });
+
+        //Code pour cliquer sur le mail et envoyer un mail
+        final TextView mail = (TextView) findViewById(R.id.mail);
+        mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",(String) mail.getText(),null));
+
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.info_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //A ajouter partout ou l'on veut que le bouton settings ouvre la page settings.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_infos:
+                Intent intent = new Intent(this, InfosApp.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 
     private void getDeviceLocation() {
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
