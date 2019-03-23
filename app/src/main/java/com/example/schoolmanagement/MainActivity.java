099f113d77e169ee95fc39398e2ceb53c2be637d
Audrey <viriotaudrey@gmail.com>
2019-03-23 13:26:51 +0100

    23.03.2019 -> Captcha ok !!

--- app/src/main/java/com/example/schoolmanagement/MainActivity.java
+++ app/src/main/java/com/example/schoolmanagement/MainActivity.java
@@ -4,16 +4,44 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences sharedPreferences;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String SITE_KEY = "6LeogJkUAAAAAG8ZY-oJlsRpFbsxMKL-pv_irl8s";
+    private static final String SITE_SECRET_KEY = "6LeogJkUAAAAAB6l9QIlc_nEqZabqATuaGP5rk5n";
+    private String userResponse;
+    private boolean successCaptcha = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +50,71 @@ public class MainActivity extends AppCompatActivity {
 
         //Bloque on vertical
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public void captchaClick(View view){
+        SafetyNet.getClient(this).verifyWithRecaptcha(SITE_KEY)
+                .addOnSuccessListener(this,
+                    new OnSuccessListener<SafetyNetApi.RecaptchaTokenResponse>() {
+                    @Override
+                    public void onSuccess(SafetyNetApi.RecaptchaTokenResponse response) {
+                        userResponse = response.getTokenResult();
+                        if(!userResponse.isEmpty()){
+                            sendRequest();
+                        }
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if(e instanceof ApiException){
+                            ApiException apiException = (ApiException) e;
+                            int statusCode = apiException.getStatusCode();
+                            Log.d(TAG,"Error : "+ CommonStatusCodes.getStatusCodeString(statusCode));
+                        }else{
+                            Log.d(TAG,"Error : "+e.getMessage());
+                        }
+                    }
+                });
+
+    }
+
+    public void sendRequest(){
+
+        String url = "https://www.google.com/recaptcha/api/siteverify";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            if (obj.getString("success").equals("true")) {
+                                successCaptcha = true;
+                                Button captchaButton = (Button) findViewById(R.id.captchaButton);
+                                captchaButton.setBackgroundColor(Color.GREEN);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Captcha failed", Toast.LENGTH_LONG).show();
+                    }
+                }) {
+
+            protected Map<String, String> getParams() throws AuthFailureError{
+                 Map<String, String> params = new HashMap<>();
+                 params.put("secret", SITE_SECRET_KEY);
+                 params.put("response", userResponse);
+                 return params;
+            }
+        };
+        AppController.getInstance(this).addToRequestQueue(stringRequest);
+
     }
 
     public void logUser(View view){
@@ -35,7 +128,8 @@ public class MainActivity extends AppCompatActivity {
 
         sharedPreferences = getSharedPreferences("key_clr", Context.MODE_PRIVATE);
 
-        if(login.equals(sharedPreferences.getString("login", "")) && mdp.equals(sharedPreferences.getString("mdp","")))
+        if(login.equals(sharedPreferences.getString("login", "")) && mdp.equals(sharedPreferences.getString("mdp",""))
+            && successCaptcha)
         {
                 Intent intent = new Intent(this, AdminSettings.class);
                 startActivity(intent);
