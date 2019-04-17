3b1f771040a3f8e6153167245ed2833335566fdf
palavina95 <vivian.bridy@students.hevs.ch>
2019-04-17 16:54:12 +0200

    17.04.2019 -> A/B testing on color buttons adminSettings

--- app/src/main/java/com/example/schoolmanagement/AdminSettings.java
+++ app/src/main/java/com/example/schoolmanagement/AdminSettings.java
@@ -8,18 +8,55 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import java.util.HashMap;
 
 public class AdminSettings extends AppCompatActivity {
 
     private SharedPreferences sharedPreferences;
+    private Button btnStudent;
+    private Button btnClass;
+
+    //Remote control
+    private FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Set developper mode for Remote control
+        remoteConfig.setConfigSettings(new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(true)
+                .build());
+
+        //Default value for remote control
+        HashMap<String, Object> defaults = new HashMap<>();
+        defaults.put("color_Student_Button","#D3D3D3");
+        defaults.put("color_Class_Button","#D3D3D3");
+        remoteConfig.setDefaults(defaults);
+
+        final Task<Void> fetch = remoteConfig.fetch(0);
+        fetch.addOnSuccessListener(this, new OnSuccessListener<Void>() {
+
+            @Override
+            public void onSuccess(Void aVoid) {
+                btnStudent = findViewById(R.id.button_manage_student);
+                btnClass = findViewById(R.id.button_manage_class);
+                remoteConfig.fetchAndActivate();
+                updateColorButtons();
+            }
+        });
 
         sharedPreferences = getSharedPreferences("key_clr", Context.MODE_PRIVATE);
         int r=sharedPreferences.getInt("a_r",0);
@@ -29,11 +66,17 @@ public class AdminSettings extends AppCompatActivity {
 
         setContentView(R.layout.activity_admin_settings);
 
-        Button btn = findViewById(R.id.button_manage_student);
-        btn.setBackgroundColor(Color.RED);
-
         //Bloque on vertical
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public void updateColorButtons(){
+        String colorStudent = remoteConfig.getString("color_Student_Button");
+        String colorClass = remoteConfig.getString("color_Class_Button");
+
+        btnStudent.setBackgroundColor(Color.parseColor(colorStudent));
+        btnClass.setBackgroundColor(Color.parseColor(colorClass));
+
     }
 
     @Override
